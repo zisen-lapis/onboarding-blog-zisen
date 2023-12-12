67ab092d260818da7154f346622e8234c5a2c6b4
Zisen Wang <zisen.wang@lapisit.com.au>
2023-12-12 14:08:37 +1100

    feat(blog): commit few stylesheet change

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { reducers, metaReducers } from './store';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideStore(reducers, { metaReducers })]
+  providers: [provideRouter(routes), provideStore(reducers, { metaReducers }), provideAnimations()]
 };
